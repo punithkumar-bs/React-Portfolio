916f29f193ca008ffbdcf5e2174d10a707872a31
pshiva <pshiva@yapstone.com>
2022-10-25 20:53:10 +0530

    Remove axios and use array for certificates

--- src/components/CertificatePage.js
+++ src/components/CertificatePage.js
@@ -3,21 +3,93 @@ import Particle from './Particle'
 import { Container, Row, Col } from "react-bootstrap";
 import Zoom from 'react-reveal/Zoom';
 import { Fade } from "react-reveal";
+import aws from '../images/aws.svg'
+import codecamp from '../images/codecamp.png'
+import google from '../images/google-logo.png'
+import hackerrank from '../images/hackerrank.png'
 import badges from '../images/badges.png'
-import axios from 'axios';
 import {useState, useEffect} from 'react'
 
 export default function ProjectPage() {
-    var [data, updatedata] = useState([]);
+    const data = [
+        {
+            "certificate_name": "AWS Cloud Practitioner Essentials",
+            "organization": "Amazon Web Services",
+            "link": "https://www.linkedin.com/feed/update/urn:li:activity:6673301367907508224/",
+            "logo": aws
+        },
+        {
+            "certificate_name": "JavaScript Algorithms and Data Structures",
+            "organization": "Free Code Camp",
+            "link": "https://www.freecodecamp.org/certification/fcc6a852a92-0af5-494d-a0c2-440a9e3774f3/javascript-algorithms-and-data-structures",
+            "logo": codecamp
+        },
+        {
+            "certificate_name": "Troubleshooting and Debugging Techniques",
+            "organization": "Google",
+            "link": "https://www.coursera.org/account/accomplishments/verify/ERXQ2J9SNSXB",
+            "logo": google
+        },
+        {
+            "certificate_name": "JavaScript (Intermediate)",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/d023b4f3b3fb",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "SQL (Intermediate)",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/b6ddad354272",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "Python",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/2213ce53505d",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "React JS",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/a8da18fe3a3c",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "Rest API",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/e2355ce20074",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "Java",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/9a0bf42ca94f",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "Javascript",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/a71119f94680",
+            "logo": hackerrank
+        },
+        {
+            "certificate_name": "SQL",
+            "organization": "Hackerrank",
+            "link": "https://www.hackerrank.com/certificates/a248166b3b25",
+            "logo": hackerrank
+        }
+    ]
     var [loading,updateloading]=useState(true);
-    async function getData() {
-        axios.get("https://punithkumar-certificates.free.beeceptor.com/my/api/path/certificate").then(result=>{
-            updateloading(false);
-            updatedata(result.data);
-    });
+
+    async function timeout(delay) {
+        return new Promise( res => setTimeout(res, delay) );
     }
+
     useEffect(() => {
-        getData();
+        timeout(2000)
+            .then(result => {
+                updateloading(false)
+            })
     }, []);
 
     return (
